c428aa193f371a95423a55fe29bbc9ed7ac3dbd2
Hana1107 <87suzuki1107@gmail.com>
2023-04-28 23:15:20 -0700

    database up and working, fully, idk, like second commit?

--- main/src/routes.py
+++ main/src/routes.py
@@ -7,15 +7,11 @@ from flask import render_template, request, redirect, url_for, session
 from run import emails, todos, users
 
 myapp_obj.secret_key = "SEAMAIL.HQ"
-loggedin = False
-current_user = ""
 
 #render index route
 @myapp_obj.route("/", methods=['GET', 'POST'])
 @myapp_obj.route("/index.html", methods=['GET', 'POST'])
 def index():
-    if loggedin:
-        return 'You are logged in as ' + session['username']
     return render_template('index.html')
 
 #add emails to database
@@ -23,9 +19,9 @@ def index():
 def sendEmail():
     if request.method == 'POST':
         emails.insert_one({'sender': 'test sender', 'subject': 'test subject', 'message': 'test message'})
-        return render_template('mailroom.html', message='message sent')
+        return render_template('mailroom.html', message='message sent', current_user = session['user'])
     else:
-        return render_template('mailroom.html', message='message not sent')
+        return render_template('mailroom.html', message='message not sent', current_user = session['user'])
 
 #list emails from database
 @myapp_obj.route('/mailroom', methods=['GET', 'POST'])
@@ -57,39 +53,55 @@ def remTodo():
         return render_template('todo.html', todos=getTodoItem)
     return render_template('todo.html', todos='Todo Not Rendered')
 
-@myapp_obj.route('/logout', methods = ['POST', 'GET'])
+@myapp_obj.route('/logout', methods = ['GET', 'POST'])
 def logout():
+    delete_user = session['user']
+    print(delete_user)
     if request.method == 'POST':
-        loggedin = False
-        current_user = ""
-        redirect(url_for('afterlogout.html'))
-
-    return render_template('logout.html')
+        if 'logout_button' in request.form:
+            session.pop('user')
+            return redirect(url_for('afterLogout'))
+        elif 'delete_button' in request.form:
+            users.delete_one({'username':delete_user})
+            session.pop('user')
+            return redirect(url_for('login'))
+    return render_template('logout.html', current_user = session['user'])
 
 @myapp_obj.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
         name = request.form['username']
         psw = request.form['password']
-        x = users.find_one({'username': name, 'password': psw})
-        if x != "":
-            loggedin = True
-            current_user = name
+        if users.find_one({'username':name, 'password':psw}) != None:
+            session['user'] = name
             return redirect(url_for('sendEmail'))
+        else:
+            return """
+            <div align = "center">
+                <h3>No account match found</h3>
+                </br>
+                <a href = 'login'>Return to login page</a>
+            </div>
+             """
     return render_template('log_in.html')
 
 @myapp_obj.route('/register', methods = ['GET', 'POST'])
 def register():
-    if request.method == 'GET':
+    if request.method == 'POST':
         new_username = request.form['username']
         new_user_password = request.form['password']
         new_user_email = request.form['email']
         new_user = {'username' : new_username, 'password' : new_user_password, 'email' : new_user_email}
-        if users.find({}, {'user': new_username}) == None:
+        if users.find_one({'username': new_username}) == None:
             users.insert_one(new_user)
+            session['user'] = new_username
             return redirect(url_for('login'))
 
-        return 'That username already exists!'
+        return """
+        <h3>That username already exists!</h3>
+        </br>
+        <a href = "register">Return to register page</a>
+        """
     return render_template('register.html')
 
 @myapp_obj.route('/afterLogout')
