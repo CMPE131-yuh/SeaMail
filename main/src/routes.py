00536d375812afcd330713b104ca4f2598331446
Marlon (Mj) Burog <spicyrice09@gmail.com>
2023-05-12 01:54:44 -0700

    Add files via upload

--- main/src/routes.py
+++ main/src/routes.py
@@ -3,11 +3,15 @@ from src import myapp_obj
 
 #import flask libraries
 from flask import render_template, request, redirect, url_for, session
-from run import emails, todos, users, socketio, chat_history, roomz
-from flask_socketio import join_room, leave_room, send, emit
-import random
-from string import ascii_uppercase
+from flask_socketio import send
 from bson.objectid import ObjectId
+from bson import Binary
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import io
+
+from run import emails, todos, users, socketio
 
 myapp_obj.secret_key = "SEAMAIL.HQ"
 
@@ -22,19 +26,66 @@ def index():
 @myapp_obj.route('/sendMail', methods=['GET', 'POST'])
 def sendEmail():
     if request.method == 'POST':
+        #if(users.find({'username': session['user'], 'blocked': True})):
+        #    sub = request.form['sub']
+        #    msg = request.form['msg']
+        #    rec = request.form['recipe']
+        #    blocked.insert_one({'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg, 'delete': False})
         sub = request.form['sub']
         msg = request.form['msg']
         rec = request.form['recipe']
-        emails.insert_one({'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg})
+        if request.files:
+            emailImage = request.files['image']
+            emails.save_file(emailImage.filename, emailImage)
+            emails.insert_one({'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg, 'delete': False, 'img': emailImage, 'hasImg': True})
+        else:
+            emails.insert_one({'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg, 'delete': False, 'hasImg': False})
         return redirect(url_for('listEmails'))
     else:
         return render_template('mailroom.html', message='message not sent', current_user = session['user'])
 
+#open image that was sent from email
+@myapp_obj.route('/openImage/<oid>', methods=['GET', 'POST'])
+def openImage(oid):
+    if request.method == 'POST':
+        #if emails.find({'_id': ObjectId(oid), 'hasImg': True}).count() > 0:
+        image = emails.find({'_id': ObjectId(oid)})
+        return render_template('viewImage.html', view=image)
+        #else:
+        #return render_template('viewImage.html', view='No Image Attached')
+
 #list emails from database
 @myapp_obj.route('/mailroom', methods=['GET', 'POST'])
 def listEmails():
-    maillist = emails.find({'username': session['user']})
-    return render_template('mailroom.html', emails=maillist, current_user = session['user'])
+    if emails.find({'username': session['user']}).count() > 0:
+        maillist = emails.find({'username': session['user']})
+        return render_template('mailroom.html', emails=maillist, current_user = session['user'])
+    else:
+        return render_template('mailroom.html', message='No Emails At This Time', current_user = session['user'])
+
+#list outbox emails from database
+@myapp_obj.route('/outbox', methods=['GET', 'POST'])
+def listOutbox():
+    maillist = emails.find({'delete': False})
+    return render_template('outbox.html', sent=maillist, current_user = session['user'])
+
+#unsend emails for both sender and receiver
+@myapp_obj.route('/unsendEmail/<oid>', methods=['GET', 'POST'])
+def unsendEmail(oid):
+    if request.method == 'POST':
+        emails.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
+        emails.delete_one({'delete': True})
+        return redirect(url_for('listOutbox'))
+    return render_template('outbox.html', sent='Error Loading Outbox')
+
+#delete email for the current user's mailroom
+@myapp_obj.route('/delEmail/<oid>', methods=['GET', 'POST'])
+def delEmail(oid):
+    if request.method == 'POST':
+        emails.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
+        emails.delete_one({'delete': True})
+        return redirect(url_for('listEmails'))
+    return render_template('mailroom.html', emails='Error Loading Mailroom')
 
 #render todolist
 @myapp_obj.route('/todolist', methods=['GET', 'POST'])
@@ -49,7 +100,7 @@ def addTodo():
         todoitem = request.form['todoitem']
         todos.insert_one({'username': session['user'], 'item': todoitem, 'delete': False})
         getTodoItem = todos.find({'username': session['user']})
-        return render_template('todo.html', todos=getTodoItem, username=session['user'])
+        return redirect(url_for('todo'))
     return render_template('todo.html', todos='Todo Not Rendered')
 
 #remove item from todo list database collection
@@ -58,13 +109,14 @@ def remTodo(oid):
     if request.method == 'POST':
         todos.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
         todos.delete_one({'delete': True})
-        getTodoItem = todos.find({'username': session['user']})
-        return render_template('todo.html', todos=getTodoItem, username=session['user'])
+        return redirect(url_for('todo'))
     return render_template('todo.html', todos='Todo Not Rendered')
 
+#logout funcionality, logs out current user session
 @myapp_obj.route('/logout', methods = ['GET', 'POST'])
 def logout():
     delete_user = session['user']
+    print(delete_user)
     if request.method == 'POST':
         if 'logout_button' in request.form:
             session.pop('user')
@@ -73,12 +125,9 @@ def logout():
             users.delete_one({'username':delete_user})
             session.pop('user')
             return redirect(url_for('login'))
-        elif 'change_password' in request.form:
-            return redirect(url_for('changePassword'))
-        elif 'change_name' in request.form:
-            return redirect(url_for('changeName'))
     return render_template('logout.html', current_user = session['user'])
 
+#login into user account, redirect into user mailroom
 @myapp_obj.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -97,13 +146,14 @@ def login():
              """
     return render_template('log_in.html')
 
+#register new user
 @myapp_obj.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
         new_username = request.form['username']
         new_user_password = request.form['password']
-        new_user_email = request.form['email'] + '@seamail.com'
-        new_user = users(new_username, new_user_password, new_user_email)
+        new_user_email = request.form['email']
+        new_user = {'username' : new_username, 'password' : new_user_password, 'email' : new_user_email, 'blocked': False}
         if users.find_one({'username': new_username}) == None:
             users.insert_one(new_user)
             session['user'] = new_username
@@ -116,91 +166,22 @@ def register():
         """
     return render_template('register.html')
 
+#logged out rendering after logging out
 @myapp_obj.route('/afterLogout')
 def afterLogout():
     return render_template('logged_out.html')
 
-@myapp_obj.route('/change_password', methods = ['GET', 'POST'])
-def changePassword():
-    if request.method == 'POST':
-        check_username = users.find_one({'username' : request.form['username'], 'password' : request.form['current_password']})
-        check_email = users.find_one({'email' : request.form['username'], 'password' : request.form['current_password']})
-        new = request.form['new_password']
+#---------------------------------------------------------
+#socket message rouonte
+@socketio.on('message')
+def handle_message(message):
+    #emit('message', json_data, broadcast=True, include_self=False)
+    print("Received Message: " + message)
+    if message != "User Connected!":
+        send(message, broadcast=True)
 
-        if check_email == None and check_username == None:
-            return """
-            <div align='center'>
-                <h3>Wrong combination of username/email and password</h3>
-                <h3>Please try again!</h3>
-                </br>
-                <a href = "change_password">Return to the previous page</a>
-            </div>
-            """
-        elif request.form['current_password'] != request.form['check_current']:
-            return """
-            <h3>The passwords don't match!</h3>
-            </br>
-            <a href = "change_password">Return to the previous page</a>
-            """
-        else:
-            query = {'username' : request.form['username']}
-            update = { "$set": { "password": new } }
-            users.update_one(query, update)
-            return redirect(url_for('logout'))
-
-    return render_template('password_change.html')
-
-@myapp_obj.route('/chatroom', methods = ['GET', 'POST'])
-def chatroom():
-    if request.method == 'POST':
-        if 'create_room' in request.form:
-            session['room'] = generate_unique_code(4)
-            roomz.insert_one({'code':session['room']})
-            session['recipient'] = request.form['recipient']
-            new_chat = {'room_num' : session['room'], 'recipient' : session['recipient'], 'sender' : session['user'], 'chats' : ['hi']}
-            chat_history.insert_one(new_chat)
-        elif 'enter_room' in request.form:
-            session['room'] = request.form['room_number']
-            session['recipient'] = request.form['recipient']
-        return redirect(url_for('room'))
-
-    return render_template('chatroom.html')
-
-@myapp_obj.route('/room', methods = ['GET', 'POST'])
-def room():
-    username = request.args.get('username')
-    room = session['room']
-    return render_template('room.html', username=username, room=room)
-
-
-#Socket.io commands ---------------------------------------------------------------------------------------------------------------------
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    myapp_obj.logger.info("{} has sent message to the room {}: {}".format(data['username'],
-                                                                    data['room'],
-                                                                    data['message']))
-    socketio.emit('receive_message', data, room=data['room'])
-
-
-@socketio.on('join_room')
-def handle_join_room_event(data):
-    myapp_obj.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
-
-
-@socketio.on('leave_room')
-def handle_leave_room_event(data):
-    myapp_obj.logger.info("{} has left the room {}".format(data['username'], data['room']))
-    leave_room(data['room'])
-    socketio.emit('leave_room_announcement', data, room=data['room'])
-
-def generate_unique_code(length):
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
-        if roomz.find_one({'code' : code}) == None:
-            break
-    return code
-
+#render messagenger route
+@myapp_obj.route('/message-room', methods=['GET', 'POST'])
+def messageRoom():
+    return render_template('message.html')
+#---------------------------------------------------------
