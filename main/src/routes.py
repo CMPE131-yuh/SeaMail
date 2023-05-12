629d1c4383049b23dda94c39fde135fc59ed37fa
stanphong <89896491+stanphong@users.noreply.github.com>
2023-05-12 10:09:12 -0700

    check_it

--- main/src/routes.py
+++ main/src/routes.py
@@ -3,14 +3,16 @@ from src import myapp_obj
 
 # import flask libraries
 from flask import render_template, request, redirect, url_for, session
+from flask_socketio import emit, send
+from bson.objectid import ObjectId
+import sys
 
-from run import emails, todos, users
+from run import emails, todos, users, socketio
 
 myapp_obj.secret_key = "SEAMAIL.HQ"
 
+
 # render index route
-
-
 @myapp_obj.route("/", methods=['GET', 'POST'])
 @myapp_obj.route("/index.html", methods=['GET', 'POST'])
 def index():
@@ -22,9 +24,17 @@ def index():
 @myapp_obj.route('/sendMail', methods=['GET', 'POST'])
 def sendEmail():
     if request.method == 'POST':
+        # if(users.find({'username': session['user'], 'blocked': True})):
+        #    sub = request.form['sub']
+        #    msg = request.form['msg']
+        #    rec = request.form['recipe']
+        #    blocked.insert_one({'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg, 'delete': False})
+        sub = request.form['sub']
+        msg = request.form['msg']
+        rec = request.form['recipe']
         emails.insert_one(
-            {'sender': 'test sender', 'subject': 'test subject', 'message': 'test message'})
-        return render_template('mailroom.html', message='message sent', current_user=session['user'])
+            {'username': rec, 'sender': session['user'], 'subject': sub, 'message': msg, 'delete': False})
+        return redirect(url_for('listEmails'))
     else:
         return render_template('mailroom.html', message='message not sent', current_user=session['user'])
 
@@ -33,15 +43,46 @@ def sendEmail():
 
 @myapp_obj.route('/mailroom', methods=['GET', 'POST'])
 def listEmails():
-    maillist = emails.find()
+    maillist = emails.find({'username': session['user']})
     return render_template('mailroom.html', emails=maillist, current_user=session['user'])
+
+# list outbox emails from database
+
+
+@myapp_obj.route('/outbox', methods=['GET', 'POST'])
+def listOutbox():
+    maillist = emails.find({'delete': False})
+    return render_template('outbox.html', sent=maillist, current_user=session['user'])
+
+# unsend emails for both sender and receiver
+
+
+@myapp_obj.route('/unsendEmail/<oid>', methods=['GET', 'POST'])
+def unsendEmail(oid):
+    if request.method == 'POST':
+        emails.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
+        emails.delete_one({'delete': True})
+        return redirect(url_for('listOutbox'))
+    return render_template('outbox.html', sent='Error Loading Outbox')
+
+# delete email for the current user's mailroom
+
+
+@myapp_obj.route('/delEmail/<oid>', methods=['GET', 'POST'])
+def delEmail(oid):
+    if request.method == 'POST':
+        emails.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
+        emails.delete_one({'delete': True})
+        return redirect(url_for('listEmails'))
+    return render_template('mailroom.html', emails='Error Loading Mailroom')
 
 # render todolist
 
 
 @myapp_obj.route('/todolist', methods=['GET', 'POST'])
 def todo():
-    return render_template('todo.html')
+    todolist = todos.find({'username': session['user']})
+    return render_template('todo.html', todos=todolist, username=session['user'])
 
 # add item to todo list database
 
@@ -50,25 +91,32 @@ def todo():
 def addTodo():
     if request.method == 'POST':
         todoitem = request.form['todoitem']
-        todos.insert_one({'item': todoitem})
-        getTodoItem = todos.find()
-        return render_template('todo.html', todos=getTodoItem)
+        todos.insert_one(
+            {'username': session['user'], 'item': todoitem, 'delete': False})
+        getTodoItem = todos.find({'username': session['user']})
+        return redirect(url_for('todo'))
     return render_template('todo.html', todos='Todo Not Rendered')
 
 # remove item from todo list database collection
 
 
-@myapp_obj.route('/removetodo', methods=['GET', 'POST'])
-def remTodo():
+@myapp_obj.route('/removetodo/<oid>', methods=['GET', 'POST'])
+def remTodo(oid):
     if request.method == 'POST':
-        todos.delete_one({})
-        getTodoItem = todos.find()
-        return render_template('todo.html', todos=getTodoItem)
+        todos.update_one({'_id': ObjectId(oid)}, {'$set': {'delete': True}})
+        todos.delete_one({'delete': True})
+        return redirect(url_for('todo'))
     return render_template('todo.html', todos='Todo Not Rendered')
+
+# logout funcionality, logs out current user session
 
 
 @myapp_obj.route('/logout', methods=['GET', 'POST'])
 def logout():
+    todolist = todos.find({'username': session['user']})
+    maillist = emails.find({'username': session['user']})
+    todos_list = list(todolist)
+    mails_list = list(maillist)
     delete_user = session['user']
     print(delete_user)
     if request.method == 'POST':
@@ -79,7 +127,9 @@ def logout():
             users.delete_one({'username': delete_user})
             session.pop('user')
             return redirect(url_for('login'))
-    return render_template('logout.html', current_user=session['user'])
+    return render_template('logout.html', current_user=session['user'], todos=todos_list, mails=mails_list)
+
+# login into user account, redirect into user mailroom
 
 
 @myapp_obj.route('/login', methods=['GET', 'POST'])
@@ -100,6 +150,8 @@ def login():
              """
     return render_template('log_in.html')
 
+# register new user
+
 
 @myapp_obj.route('/register', methods=['GET', 'POST'])
 def register():
@@ -107,8 +159,8 @@ def register():
         new_username = request.form['username']
         new_user_password = request.form['password']
         new_user_email = request.form['email']
-        new_user = {'username': new_username,
-                    'password': new_user_password, 'email': new_user_email}
+        new_user = {'username': new_username, 'password': new_user_password,
+                    'email': new_user_email, 'blocked': False}
         if users.find_one({'username': new_username}) == None:
             users.insert_one(new_user)
             session['user'] = new_username
@@ -121,7 +173,28 @@ def register():
         """
     return render_template('register.html')
 
+# logged out rendering after logging out
+
 
 @myapp_obj.route('/afterLogout')
 def afterLogout():
     return render_template('logged_out.html')
+
+# ---------------------------------------------------------
+# socket message rouonte
+
+
+@socketio.on('message')
+def handle_message(message):
+    # emit('message', json_data, broadcast=True, include_self=False)
+    print("Received Message: " + message)
+    if message != "User Connected!":
+        send(message, broadcast=True)
+
+# render messagenger route
+
+
+@myapp_obj.route('/message-room', methods=['GET', 'POST'])
+def messageRoom():
+    return render_template('message.html')
+# ---------------------------------------------------------
