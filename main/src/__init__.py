5cf5b9820fb0529b31366519e599ba56ce642931
Marlon (Mj) Burog <spicyrice09@gmail.com>
2023-05-17 11:11:54 -0700

    Update __init__.py

--- main/src/__init__.py
+++ main/src/__init__.py
@@ -1,5 +1,8 @@
+#import flask for init
 from flask import Flask
 
+#initialize flask application as myapp_obj to be used in other backend pythonn files
 myapp_obj = Flask(__name__)
 
+#import routes for myapp_obj
 from src import routes
