df10fd281886aa12c733e8b0404f4b15c7767e3b
Marlon (Mj) Burog <spicyrice09@gmail.com>
2023-05-17 11:09:41 -0700

    Update run.py

--- main/run.py
+++ main/run.py
@@ -1,18 +1,23 @@
-#turn on application with debug on using flask
+#imports for pymongo to connect application to external database through pymongo api
 from dotenv import dotenv_values
 from flask_pymongo import pymongo
 from pymongo.mongo_client import MongoClient
 from flask_socketio import SocketIO, emit
 import gridfs
 
+#import app object from init py file to be able to run the application
 from src import myapp_obj
 
+#config to be able to get uri and other information from .env file
 config = dotenv_values(".env")
 
+#uri link to connect to external mongo database
 MONGO_URI = 'mongodb+srv://hanasuzuki:8gZpPyV7ZkTb7XOi@test.6mtzohb.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE'
 
+#socketio initialization to run with app object from init py
 socketio = SocketIO(myapp_obj, cors_allowed_origins="*")
 
+#connection to mongoDB client and connecting to each collection in the SeaMail databaase to intialize them as reusable objects
 mongo = MongoClient(MONGO_URI)
 db = mongo.get_database('SeaMail')
 emails = pymongo.collection.Collection(db, 'emails')
@@ -24,7 +29,9 @@ blocked = pymongo.collection.Collection(db, 'blocked')
 blockedEmails = pymongo.collection.Collection(db, 'blockedEmails')
 image = pymongo.collection.Collection(db, 'images')
 
+#gridfs for file download
 grid_fs = gridfs.GridFS(db)
 
+#start application on localhost under socketio
 if __name__ == '__main__':
     socketio.run(myapp_obj, debug = True)
