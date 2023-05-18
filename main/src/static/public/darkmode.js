d4c55512c86380d39f6333db783a051674adf0af
stanphong <89896491+stanphong@users.noreply.github.com>
2023-05-17 20:16:38 -0700

    add comments

--- main/src/static/public/darkmode.js
+++ main/src/static/public/darkmode.js
@@ -1,38 +1,56 @@
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    var myTables = document.querySelectorAll(".myTable");
-    myTables.forEach(function(myTable){
-      myTable.classList.toggle("table-dark");
+  // variable that points to the body tag
+  var element = document.body;
+  // add the "dark-mode" class to this element
+  element.classList.toggle("dark-mode");
+
+  // variable that points to all table that have ".myTable" tag
+  var myTables = document.querySelectorAll(".myTable");
+
+  // for each table, add the "table-dark" class from Bootstrap
+  myTables.forEach(function(myTable){
+    myTable.classList.toggle("table-dark");
+  });
+
+  // variable points to the checkbox (the toggle) that has id "customSwitches"
+  var checkbox = document.getElementById("customSwitches");
+
+  // Variable points to all the buttons that has ".my-button" class
+  var buttons = document.querySelectorAll(".my-button");
+  
+  // If else cases whether the checkbox is checked to remove or add "btn-outline-dark" and "btn-outline-light"
+  if (checkbox.checked) {
+    buttons.forEach(function(button) {
+      button.classList.remove("btn-outline-dark");
+      button.classList.add("btn-outline-light");
     });
-    var checkbox = document.getElementById("customSwitches");
-    var buttons = document.querySelectorAll(".my-button");
-  
-    if (checkbox.checked) {
-      buttons.forEach(function(button) {
-        button.classList.remove("btn-outline-dark");
-        button.classList.add("btn-outline-light");
-      });
-    } else {
-      buttons.forEach(function(button) {
-        button.classList.remove("btn-outline-light");
-        button.classList.add("btn-outline-dark");
-      });
-    }
-  
-    var links = document.querySelectorAll(".anchor");
-    links.forEach(function(link) {
-      link.classList.toggle("text-warning", checkbox.checked);
+  } 
+  else {
+    buttons.forEach(function(button) {
+      button.classList.remove("btn-outline-light");
+      button.classList.add("btn-outline-dark");
     });
-  
-    var divs = document.querySelectorAll(".my-div");
-    divs.forEach(function(div) {
-      div.classList.toggle("bg-transparent")
-    });
-  
-    // Store the current theme preference in localStorage
-    localStorage.setItem('theme', element.classList.contains("dark-mode") ? 'dark' : 'light');
   }
+  
+  // variable that points to all anchor tag that has ".anchor" class
+  var links = document.querySelectorAll(".anchor");
+
+  // for each anchor tag, add the "text-warning" class when the checkbox checked
+  links.forEach(function(link) {
+    link.classList.toggle("text-warning", checkbox.checked);
+  });
+  
+  // variable that points to all div tag that has ".my-div" class
+  var divs = document.querySelectorAll(".my-div");
+
+  // for each div, add the "bg-transparent" class when the checkbox checked
+  divs.forEach(function(div) {
+    div.classList.toggle("bg-transparent")
+  });
+  
+  // Store the current theme preference in localStorage
+  localStorage.setItem('theme', element.classList.contains("dark-mode") ? 'dark' : 'light');
+}
   
   
   
